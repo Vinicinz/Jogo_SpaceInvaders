47163e6ce9852da898eeb70ac2c01625f68f42ce
Your Name <you@example.com>
2024-05-28 10:04:53 -0300

    Classe inimigo criada e adição dela na fase + comentarios

--- src/Fase.java
+++ src/Fase.java
@@ -8,7 +8,9 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -27,6 +29,7 @@ public class Fase extends JPanel implements ActionListener {
     private Player player;
     private Timer timer;
     private Clip clip;
+    private List <Enemy1> enemy1; 
 
     // Construtor Fase
     public Fase() {
@@ -53,9 +56,11 @@ public class Fase extends JPanel implements ActionListener {
         } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
         }
 
-        //criando o player e o timer pra fazer rodar
+        //criando o player, iniciando o inimigo na fase e o timer pra fazer da update na fase
         player = new Player();
         player.load();
+
+        inicializaInimigo();
 
         addKeyListener(new TecladoAdapter());
 
@@ -63,14 +68,36 @@ public class Fase extends JPanel implements ActionListener {
         timer.start();
     }
 
+    // metodo que define previamente a posição de todos os inimigos no inicio da fase, ela cria uma lista de 40 inimigos e posiciona eles fora da fase
+    public void inicializaInimigo(){
+        int cordenadas[] = new int [40];
+        enemy1 = new ArrayList<Enemy1>();
+
+        for (int i = 0; i < cordenadas.length; i++) {
+            int x = (int)(Math.random()* 8000 + 1024);
+            int y = (int)(Math.random()* 650 + 30);
+            enemy1.add(new Enemy1(x, y));            
+        }
+    }
+
     // Paint component é importante pra mostrar oq ta rolando e acontecendo na fase
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D graficos = (Graphics2D) g;
+        // definindo um fundo estatico
         graficos.drawImage(fundo1, 0, 0, null);
+        // definindo que o player recebe posições de acordo com as mudanças na classe player
         graficos.drawImage(player.getImagem(), player.getX(), player.getY(), this);
 
+        // laço para atualiza a movimentação do inimigo
+        for (int j = 0; j < enemy1.size(); j++) {
+            Enemy1 in = enemy1.get(j);
+            in.load();
+            graficos.drawImage(in.getImagem(), in.getX() , in.getY(), this);
+        }
+
+        //laço para atualizar a movimentação do tiro
         List<Tiro> tiros = player.getTiros();
         for (int i = 0; i < tiros.size(); i++) {
             Tiro m = tiros.get(i);
@@ -95,9 +122,17 @@ public class Fase extends JPanel implements ActionListener {
             }
         }
 
+        for (int o = 0; o < enemy1.size(); o++) {
+            Enemy1 in = enemy1.get(o);
+            if (in.isVisivel()) {
+                in.update();
+            } else {
+                enemy1.remove(o);
+            }
+        }
+
         repaint();
     }
-
 
     // Declarando o Teclado adapter pra minha fase entender quando eu precionar as teclas
     private class TecladoAdapter extends KeyAdapter {
@@ -130,6 +165,14 @@ public class Fase extends JPanel implements ActionListener {
 
     public void setTimer(Timer timer) {
         this.timer = timer;
+    }
+
+    public List<Enemy1> getEnemy1() {
+        return enemy1;
+    }
+
+    public void setEnemy1(List<Enemy1> enemy1) {
+        this.enemy1 = enemy1;
     }
 
 }
